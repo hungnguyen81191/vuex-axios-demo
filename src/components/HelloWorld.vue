2542c794045f49d68d32fe15e4d92ba9e805e5a2
hung <hungnguyen81191@gmail.com>
2022-06-23 13:21:11 +0700

    get user use pinia demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,45 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div v-for="fUser in piniaUser" :key="fUser.id">
+      {{ getterUser.id }} {{ getterUser.name }} {{ getterUser.address }}
+    </div>
+    <h1>dai phan cach</h1>
+    <div v-for="user in user" :key="user.id">
+      {{ user.id }} {{ user.name }} {{ user.address }}
+    </div>
   </div>
 </template>
 
 <script>
+import { ref } from "vue";
+import { storeToRefs } from "pinia";
+import axios from "axios";
+//import users store
+import { useUserStore } from "../store/users";
+// declare store variable
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  setup() {
+    const store = useUserStore();
+    const msg = ref("Welcome to my Vuex Store");
+    // const getUsers = computed(() => {
+    //   return store.getUsers;
+    // });
+    const { fetchUser } = store;
+    const { piniaUser } = storeToRefs(store);
+    // onMounted(() => {
+    //   store.fetchUser();
+    // });
+    return { msg, fetchUser, piniaUser };
+  },
+  data() {
+    return {
+      user: [],
+    };
+  },
+  async created() {
+    const data = await axios.get("https://jsonplaceholder.typicode.com/users");
+    this.user = data.data;
+  },
+};
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
